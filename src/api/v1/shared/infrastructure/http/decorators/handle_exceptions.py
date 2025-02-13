c7a7deb7733bbd3cbd6ce7cd9372ed61fa920abb
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2025-02-13 18:17:59 -0300

    refactor(infrastructure): :recycle: Se cambio el control de errores a shared

--- src/api/v1/shared/infrastructure/http/decorators/handle_exceptions.py
+++ src/api/v1/shared/infrastructure/http/decorators/handle_exceptions.py
@@ -2,20 +2,21 @@ from typing import Any, Awaitable, Callable, TypeVar
 
 from fastapi import HTTPException
 
-from src.api.v1.notes.domain.errors.tags import TagError
 from src.api.v1.shared.domain.errors.shared_error import SharedError
+from src.api.v1.user.domain.errors.user_error import UserError
+from src.api.v1.notes.domain.errors.notes import NoteError
 
 T = TypeVar("T")
 
 
 def handle_exceptions(func: Callable[..., Awaitable[T]]) -> Callable[..., Awaitable[T]]:
-    """Decorador para manejar excepciones específicas de Tags y generales."""
+    """Decorador para manejar excepciones y evitar repetición de código."""
 
     async def wrapper(*args: Any, **kwargs: Any) -> T:
         try:
             return await func(*args, **kwargs)
-        except (TagError, SharedError) as e:
-            raise HTTPException(status_code=404, detail=str(e))
+        except (UserError, SharedError, NoteError) as e:
+            raise HTTPException(status_code=400, detail=str(e))
         except Exception as e:
             raise HTTPException(
                 status_code=500, detail=f"Error interno del servidor: {e}"
