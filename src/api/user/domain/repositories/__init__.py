1d5673e9f21d3f2134516a8c5968a6ee589d16b6
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2025-02-25 10:46:18 -0300

    docs(domain): :memo: Se agrego stringdocs de repository de user

--- src/api/user/domain/repositories/__init__.py
+++ src/api/user/domain/repositories/__init__.py
@@ -1,3 +1,10 @@
+"""
+Initialization module for the user domain repositories package.
+
+This module imports and exposes the UserRepository interface, which defines the methods
+necessary for managing and persisting user entities in the domain.
+"""
+
 from .user_repository import UserRepository
 
 __all__ = ["UserRepository"]
