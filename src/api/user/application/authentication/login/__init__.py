847353b1f38eee2864f50e58f8ddf6d5901e5f11
Giuliano Ignacio Poeta <giuliano.poeta.laboral@gmail.com>
2025-02-26 09:52:51 -0300

    docs(application): :memo: Se agregaron stringdocos la caso de uso de login de user

--- src/api/user/application/authentication/login/__init__.py
+++ src/api/user/application/authentication/login/__init__.py
@@ -1,3 +1,13 @@
+"""
+Initialization module for the login use case in the user application.
+
+This module imports and exposes the necessary components for user authentication,
+specifically the DTO (LoginDTO) and the use case (LoginUseCase) for the login process.
+The __all__ attribute is used to define the names that will be exported when the package
+is imported.
+"""
+
+from .login_dto import LoginDTO
 from .login_use_case import LoginUseCase
 
-__all__ = ["LoginUseCase"]
+__all__ = ["LoginDTO", "LoginUseCase"]
